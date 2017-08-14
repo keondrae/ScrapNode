6a5e5a4448fce1410e5139e8bad7cb54431f03f8
Ke'Ondrae <kmell@TechnifabInc.com>
2017-08-14 14:38:49 -0400

    Fixed On Chart Click

--- public/javascripts/plantOne.js
+++ public/javascripts/plantOne.js
@@ -48,7 +48,7 @@ function PlantOneAllDetails(data) {
         seriesGroups: [
             {
                 type: "column",
-                click: onChartClickTest,
+                click: onChartClick,
                 series: [
                     {
                         dataField: "AllPlantOneEndingBalance", displayText: 'All Plant One'
@@ -360,52 +360,3 @@ function PlantOneOtherDetails(data) {
 
 }
 
-function onChartClickTest(e) {
-    var id;
-    var thisDate;
-    var thisYear = Year;
-    var PrevYear = parseInt(thisYear) - 1;
-    $('#GridForEverything').jqxGrid('removefilter', 'MonthDate');
-    id = e.elementIndex;
-    console.log(id);
-    switch (id) {
-        case 0:
-            thisDate = '1-Dec-' + PrevYear;
-            break;
-        case 1:
-            thisDate = '1-Jan-' + thisYear;
-            break;
-        case 2:
-            thisDate = '1-Feb-' + thisYear;
-            break;
-        case 3:
-            thisDate = '1-Mar-' + thisYear;
-            break;
-        case 4:
-            thisDate = '1-Apr-' + thisYear;
-            break;
-        case 5:
-            thisDate = '1-May-' + thisYear;
-            break;
-        case 6:
-            thisDate = '1-Jun-' + thisYear;
-            break;
-        case 7:
-            thisDate = '1-Jul-' + thisYear;
-            break;
-        case 8:
-            thisDate = '1-Aug-' + thisYear;
-            break;
-        case 9:
-            thisDate = '1-Sep-' + thisYear;
-            break;
-        case 10:
-            thisDate = '1-Oct-' + thisYear;
-            break;
-        case 11:
-            thisDate = '1-Nov-' + thisYear;
-            break;
-    }
-    console.log(thisDate);
-    //addfilter(thisDate);
-}
